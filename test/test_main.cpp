e5832a19a51c68743a54c3cf2b468959ff746ef5
Михаил Лугин <329898@mail.ru>
2018-09-29 22:15:07 +0300

    3

--- test/test_main.cpp
+++ test/test_main.cpp
@@ -2,7 +2,8 @@
 
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
-	system("pause");
-    return RUN_ALL_TESTS();
-	system("pause");
+	//system("pause");
+    
+	 system("pause");
+	return  RUN_ALL_TESTS();
 }
